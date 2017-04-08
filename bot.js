932b13a1bba058cbe58fbacef37839f3a9bf0eda
Ken Luo <luozhirong@gmail.com>
2017-04-07 18:41:33 -0700

    added some more code to do some other stuff

--- bot.js
+++ bot.js
@@ -1,62 +1,79 @@
 var builder = require('botbuilder');
 var request = require('request');
 
-// var holder = request('http://api.wunderground.com/api/4863d9fbca8e5290/conditions/q/CA/Los_Angeles.json', function(err, response, body){
-// //    console.log(body);
-//     var h = JSON.parse(body);
-
-//     console.log(h.current_observation.temperature_string);
-// });
-
-//console.log(h.temp_f);
-//console.log(h);
+// imgfliplogin
+// usr: previewdaybot
+// password: mememe
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 
+//var dialog = new builder.DialogAction(connector);
+
 bot.dialog('/', [
     function(session){
-    	session.beginDialog('/askName');
+	session.beginDialog('/intro');
+    },
+    function(session){
+    	session.beginDialog('/getWeather');
     },
     function(session, results){
     	session.send('Hello %s!', results.response);
     }
-    // function(session){
-    // 	builder.Prompts.text(session, 'Hi! whats yo name');
-    // },
-    // function(session, results){
-    // 	session.send('hello ' + results.response);
-    // }
 ]);
 
+// var memeGen = request('https://api.imgflip.com/get_memes', function(err, response, body){
+//     console.log(body);
+// });
+// var memes = request('https://api.imgflip.com/caption_image', function(err, response, body){
+//     console.log(body);
+// });
 
 var holder = request('http://api.wunderground.com/api/4863d9fbca8e5290/conditions/q/CA/Los_Angeles.json', function(err, response, body){
     var h = JSON.parse(body);
-
-    console.log(h.current_observation.temperature_string);
-
-
-    bot.dialog('/askName', [
-    function(session){
-	builder.Prompts.text(session, 'wanna know the weather??');
-    },
-    function(session, results){
-	if(results.response == 'Yes'){
-	    session.send(h.current_observation.temperature_string);
+//    console.log(h.current_observation.temperature_string);
+    bot.dialog('/getWeather', [
+	function(session){
+    	    builder.Prompts.text(session, 'wanna know the weather??');
+	},
+	function(session, results){
+    	    if(results.response.toLowerCase() == 'yes'){
+    		builder.Prompts.text(session, h.current_observation.temperature_string);
+    	    }
+    	    else{
+    		session.send('Screw you then!');
+    	    }
 	}
-	else{
-	    session.send('Screw you then!');
-	}
-    }
     ]);
 });
-// bot.dialog('/askName', [
-//     function(session){
-// 	builder.Prompts.text(session, 'Hi! What is your name?');
+
+
+bot.dialog('/intro', [
+    function(session){
+	    builder.Prompts.text(session,'Whats up dude');
+    },
+    function(session, results){
+	session.endDialogWithResult(results);
+    }
+]);
+
+
+
+
+
+// bot.dialog('/', [
+//     function (session) {
+//         session.beginDialog('/askName');
 //     },
-//     function(session, results){
-// 	session.endDialogWithResult(results);
+//     function (session, results) {
+//         session.send('Hello %s!', results.response);
 //     }
 // ]);
-
-
+// bot.dialog('/askName', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         session.endDialogWithResult(results);
+//     }
+// ]);
